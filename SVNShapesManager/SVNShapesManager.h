d369e860ed0a1f5fc9455982571b67ee4992171f
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-05 14:30:21 -0700

    Initial Commit

---
+++ SVNShapesManager/SVNShapesManager.h
@@ -0,0 +1,19 @@
+//
+//  SVNShapesManager.h
+//  SVNShapesManager
+//
+//  Created by Aaron Dean Bikis on 4/5/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNShapesManager.
+FOUNDATION_EXPORT double SVNShapesManagerVersionNumber;
+
+//! Project version string for SVNShapesManager.
+FOUNDATION_EXPORT const unsigned char SVNShapesManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNShapesManager/PublicHeader.h>
+
+
